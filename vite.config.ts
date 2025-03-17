4254fd33747f7ca09b1db5a5dd964f7dce091153
mohdyawars <mohdyawars@gmail.com>
2025-03-17 21:44:34 +0530

    fix: updated merge pdf for prod 5

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: false
+            }
+        }
+    }
 });
